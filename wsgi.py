0c4026cf45e9918aa492dd31e3d9bcac50634f58
Abdulbasit110 <BASIT4408944@cloud.neduet.edu.pk>
2025-02-01 18:03:28 +0500

    update requirements to include gunicorn and eventlet; allow unsafe Werkzeug in wsgi for development

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,4 @@ from app import create_app, socketio
 app = create_app()  # Create Flask app using factory pattern
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, host="0.0.0.0", port=5000)  # Run Flask with SocketIO
+    socketio.run(app, debug=True, host="0.0.0.0", port=5000,allow_unsafe_werkzeug=True)  
