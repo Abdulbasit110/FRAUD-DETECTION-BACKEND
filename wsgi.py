f336fa52e10c943dfbb01eabf0823c6967729862
Abdulbasit110 <BASIT4408944@cloud.neduet.edu.pk>
2025-02-01 18:22:05 +0500

    update requirements to include waitress; import eventlet in wsgi and adjust socketio run configuration

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 from app import create_app, socketio
+import eventlet
 
-app = create_app()  # Create Flask app using factory pattern
+app = create_app()
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, host="0.0.0.0", port=5000,allow_unsafe_werkzeug=True)  
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
